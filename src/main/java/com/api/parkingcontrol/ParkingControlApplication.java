c56befc50f8a05e29721b3ea7c5be238e988a650
Lucas Costa <lukascosta.ls17@gmail.com>
2025-03-21 17:03:51 -0300

    Update

--- src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -10,10 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ParkingControlApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ParkingControlApplication.class, args);
-	}
-	@GetMapping("/")
-	public String index(){
-		return "Olá Mundo!";
 	}
 }
